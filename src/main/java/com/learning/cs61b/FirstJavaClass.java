9e6d06dfca5f85c7bbe20575fb919b5f58a94b21
Bin Jiang <binjiang_ca@hotmail.com>
2021-08-28 20:29:12 -0400

    bin update code

--- src/main/java/com/learning/cs61b/FirstJavaClass.java
+++ src/main/java/com/learning/cs61b/FirstJavaClass.java
@@ -3,5 +3,6 @@ package com.learning.cs61b;
 public class FirstJavaClass {
     public static void main(String[] args){
         System.out.println("Hello World...");
+        System.out.println("Bin Hello World...");
     }
 }
